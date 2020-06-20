19af85e8007c144eb36fd16dfd81ebe11ac110c7
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-06-20 16:04:11 -0700

    add functionality

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+let createArray = function(input) {
+    let integerArray = [];
+    if (input > 0) {
+        for (let i = input; i >= 0; i -= 1) {
+            integerArray.push(parseInt(i));
+        }
+        return integerArray.reverse(); 
+    } else {
+        $("#output").text("Sorry! Please enter a positive non-zero number.");
+    }
+}
+
+let parseNum = function(inputNumber) {
+    if ( inputNumber.toString().indexOf('3') > -1 ) {
+        return "Won't you be my neighbor?";
+    } else if ( inputNumber.toString().indexOf('2') > -1 ) {
+        return "Boop!";
+    } else if ( inputNumber.toString().indexOf('1') > -1 ) {
+        return "Beep!";
+    } else {
+        return inputNumber;
+    }
+}
+
+let robogersNeighborhood = function(inputNumber) {
+    let integerArray = createArray(inputNumber);
+    let resultsArray = [];
+    integerArray.forEach(function(index) {
+        resultsArray.push(parseNum(integerArray[index]));
+        // console.log(parseNum(integerArray[index]));
+    })
+    return resultsArray;
+}
+
+$(document).ready(function() {
+    $("#user-input").submit(function(event) {
+        event.preventDefault();
+        let inputNumber = $("#input-number").val();
+        $("#input-number").val("");
+        let result = robogersNeighborhood(inputNumber);
+
+        $("#output").text(result);
+    });
+});
+
+// window.onLoad = function() {
+    
+    // var form_el = document.getElementById("#user-input");
+    
+    // form_el.addEventListener("submit", function(event) {
+    //     event.preventDefault();
+    //     var inputNumber = document.getElementById("input-number");
+    //     console.log(inputNumber, "made it");
+        
+    //     createArray(inputNumber);
+    // });
+// };
